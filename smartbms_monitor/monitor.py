527d159fb78ecd40ec485382a7ccb44d10f184af
tomcatling <tomcatling@gmail.com>
2021-02-23 11:47:25 +0000

    Adding monitor logic

--- smartbms_monitor/monitor.py
+++ smartbms_monitor/monitor.py
@@ -2,15 +2,27 @@ import serial
 
 from utils import decode 
 
-# port = serial.Serial(
-#     "/dev/ttyS0", 
-#     parity=serial.PARITY_NONE, 
-#     stopbits=serial.STOPBITS_ONE,
-#     baudrate=9600, 
-#     timeout=3.0
-# )
+port = serial.Serial(
+    "/dev/ttyS0", 
+    parity=serial.PARITY_NONE, 
+    stopbits=serial.STOPBITS_ONE,
+    baudrate=9600, 
+    timeout=3.0
+)
 
-# while True:
-#     rcv = port.read()
-#     print(rcv)
+
+
+while True:    
+
+    # wait for start bit
+    while port.read() != b'\x01':
+        pass
+
+    rcv = bytearray(b'')
+    while len(rcv) < 58:
+        rcv.append(port.read())
+
+    output = decode(rcv)
+
+    print(output)
 
